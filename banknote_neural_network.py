309accf31de7a058a362af4826367f926aedaa5b
Wojciech Turek <turekwojtek@gmail.com>
2022-12-12 17:51:25 +0100

    Changed to tensorflow and banknotes auth implemented

--- banknote_neural_network.py
+++ banknote_neural_network.py
@@ -1,10 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from numpy import genfromtxt
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
 
 """
 Authors:
@@ -12,36 +8,20 @@ Authors:
 - Turek Wojciech - s21611
 """
 
-# data = genfromtxt('data_banknote_authentication.csv', delimiter=',', skip_header=1)
-# X, y = data[:, :2], data[:, -1]
+"""
+    Banknotes authentication classifying
+"""
+print("Banknotes authentication")
+b_data = pd.read_csv('data_banknote_authentication.csv', names=['variance', 'skewness', 'kurtosis', 'entropy', 'class'])
+b_train_data = b_data.copy()
+b_train_label = b_data.pop('class')
+b_train_data = np.array(b_train_data)
 
-dataset = pd.read_csv("data_banknote_authentication.csv")
-print(dataset.head(5))
-x, y = dataset.drop('class', axis=1), dataset['class']
-
-# Separate input data into two classes based on labels
-# class_0 = np.array(X[y==0])
-# class_1 = np.array(X[y==1])
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=5)
-
-mlp = MLPClassifier(hidden_layer_sizes=(50,), max_iter=10, alpha=1e-4,
-                    solver='sgd', verbose=10, random_state=1,
-                    learning_rate_init=.1)
-
-mlp.fit(X_train, y_train)
-
-print("Training set score: %f" % mlp.score(X_train, y_train))
-print("Test set score: %f" % mlp.score(X_test, y_test))
-fig, axes = plt.subplots(4, 4)
-
-# use global min / max to ensure all weights are shown on the same scale
-vmin, vmax = mlp.coefs_[0].min(), mlp.coefs_[0].max()
-
-for coef, ax in zip(mlp.coefs_[0].T, axes.ravel()):
-    ax.matshow(coef.reshape(28, 5), cmap=plt.cm.gray, vmin=.5 * vmin, vmax=.5 * vmax)
-    ax.set_xticks(())
-    ax.set_yticks(())
-
-print("DUPSĄ")
-plt.show()
+b_model = tf.keras.Sequential([tf.keras.layers.Dense(64, activation='relu'),
+                                      tf.keras.layers.Dense(1)
+                                      ])
+b_model.compile(optimizer='adam',
+                       loss='mean_squared_error',
+                       metrics=['accuracy']
+                       )
+b_model.fit(b_train_data, b_train_label, epochs=7)
